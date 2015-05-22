4898883cb7d5b3241ed789ae966e9866e618a7ce
T-107 <T-107@PC242>
2015-05-21 19:43:20 -0500

    Segunda clase

---
+++ LenguajeICR/src/abstraccion/InterfazImc.java
@@ -0,0 +1,16 @@
+package abstraccion;
+
+/**
+ *
+ * @author T-107
+ */
+public class InterfazImc {
+ 
+    public static void main(String[]oso){
+        
+        Imc imc=new Imc(80,1.67f);
+        System.out.println(imc.calcular());
+
+  
+    }
+}
